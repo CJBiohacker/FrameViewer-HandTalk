17140c7a34bbd7d1ce8970156101c0b472952b60
CJBiohacker <cj.moyses@gmail.com>
2024-03-29 03:16:07 -0300

    feat/style: create responsive flex frame layout

--- Community/frame-viewer/src/components/ViewPage.tsx
+++ Community/frame-viewer/src/components/ViewPage.tsx
@@ -15,12 +15,28 @@ const ViewPage: React.FC = () => {
     getFrames();
   }, [id]);
 
+  const elementStyles = {
+    title: "tw-text-center tw-text-2xl tw-font-bold tw-mb-10 tw-my-4",
+    container: "tw-flex tw-flex-wrap tw-justify-center tw-gap-5",
+    frameContainer:
+      "tw-border-violet-500 tw-border-solid tw-border-2 tw-p-2 relative cursor-pointer",
+    image: "tw-w-64 tw-border-solid tw-border-2 tw-border-black tw-rounded-sm",
+  };
+
   return (
     <div>
-      <h2>Frames do Vídeo {id}</h2>
-      <div>
-        {frames.map((frame:unknown, index:Key) => (
-          <img key={index} src={frame.url} alt={`Frame ${index}`} />
+      <h2 className={elementStyles.title}>Frames do Vídeo de ID: {id}</h2>
+      <div className={elementStyles.container}>
+        {frames.map((frame: unknown, index: Key) => (
+          <figure className={elementStyles.frameContainer}>
+            <img
+              key={index}
+              src={frame}
+              alt={`Frame ${index}`}
+              className={elementStyles.image}
+            />
+            <figcaption className="tw-text-center">Frame {index}</figcaption>
+          </figure>
         ))}
       </div>
     </div>
