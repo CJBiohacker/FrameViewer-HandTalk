5e40876b12455bff23d6ad9332e2cb62dc7f0302
CJBiohacker <cj.moyses@gmail.com>
2024-03-29 03:14:35 -0300

    fix: adjust the url concatenation bug

--- Community/frame-viewer/src/utils/api.ts
+++ Community/frame-viewer/src/utils/api.ts
@@ -1,12 +1,12 @@
 import Video from "../types/types-and-interfaces";
 
-const API_URL = "http://localhost:3001"; // Replace with your backend URL
+// const API_URL = import.meta.env.VITE_API_URL; // Replace with your backend URL
 
 export const uploadVideo = async (file: File) => {
   const formData = new FormData();
   formData.append("video", file);
 
-  const response = await fetch(`${API_URL}/upload`, {
+  const response = await fetch(`/api/upload`, {
     method: "POST",
     body: formData,
   });
@@ -16,14 +16,14 @@ export const uploadVideo = async (file: File) => {
 };
 
 export const fetchVideosMetadata = async (): Promise<Video[]> => {
-  const response = await fetch(`${API_URL}/list`);
+  const response = await fetch(`/api/list`);
   const data = await response.json();
 
   return data;
 };
 
 export const fetchFrames = async (videoId: string): Promise<string[]> => {
-  const response = await fetch(`${API_URL}/list/frames/${videoId}`);
+  const response = await fetch(`/api/list/frames/${videoId}`);
   const data = await response.json();
 
   return data;
